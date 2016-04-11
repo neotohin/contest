3ae4d11678868ab229d469b30b0ca79009ca198f
EBashkoff <eric@fivestreet.com>
2016-04-11 08:56:38 -0400

    Fix database backup script so it uploads to S3 with AWS creds.  Remove CSV contents from repo.

--- script/backup.sh
+++ script/backup.sh
@@ -5,6 +5,10 @@ heroku pg:backups capture --app bashki-contest
 DD=`date '+%Y-%m-%d-%X.dump'`
 curl -o $DD `heroku pg:backups public-url --app bashki-contest`
 echo Uploading file to Amazon S3 cloud storage...
-aws s3 cp $DD s3://edible-2016-contest-backups/ --grants full=id=67da709c110895e2a3a27820685f8ad8837cc38cd6313d13329220ee8d535088
+AWS_ACCESS_KEY=`heroku config:get AWS_ACCESS_KEY`
+AWS_SECRET_KEY=`heroku config:get AWS_SECRET_KEY`
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
+aws s3 cp $DD s3://edible-2016-contest-backups/
 echo Removing temporary local backup file...
 rm $DD
